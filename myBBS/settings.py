bf348992afd1d4fdaab529bcd72aca66c69c9eed
lover-520 <wzm000821@163.com>
2020-03-20 21:12:07 +0800

    March 20, 2020 initial database

--- myBBS/settings.py
+++ myBBS/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'boards',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +78,12 @@ WSGI_APPLICATION = 'myBBS.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'bbs',
+        'USER': 'bbs_admin',
+        'PASSWORD': '000821',
+        'HOST': '%',
+        'POrt': '3308',
     }
 }
 
