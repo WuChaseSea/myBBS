fe1abf8ec269fd7e5c6852ddede050bd79ffe277
lover-520 <wzm000821@163.com>
2020-03-23 20:00:45 +0800

    March 23, 2020 Models builded and migrated

--- myBBS/settings.py
+++ myBBS/settings.py
@@ -77,13 +77,12 @@ WSGI_APPLICATION = 'myBBS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'bbs',
         'USER': 'bbs_admin',
         'PASSWORD': '000821',
-        'HOST': '%',
-        'POrt': '3308',
+        'Host': '%',
+        'PORT': '3308',
     }
 }
 
